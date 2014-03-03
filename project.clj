169e7f7999d0237e1bd4e0059c650d6cfa96a840
Malcolm Sparks <malcolm@juxt.pro>
2014-03-03 16:02:35 +0000

    Deployment main

--- project.clj
+++ project.clj
@@ -1,8 +1,12 @@
 (defproject kixi/stentor "0.1.0-SNAPSHOT"
+
   :description "Stentor is the herald of the Greeks"
+
   :url "http://github.com/mastodonc/kixi.stentor"
+
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
+
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [juxt/modular "0.1.0-SNAPSHOT"]
                  ;; EDN reader with location metadata
@@ -10,4 +14,6 @@
                  [juxt/modular.http-kit "0.1.0-SNAPSHOT"]
                  ]
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]
-                   :source-paths ["dev"]}})
+                   :source-paths ["dev"]}
+             :uberjar {:main kixi.stentor.main
+                       :aot [kixi.stentor.main]}})
---
+++ src/kixi/stentor/main.clj
@@ -0,0 +1,9 @@
+(ns kixi.stentor.main
+  (:gen-class)
+  )
+
+(defn -main [& args]
+  (eval '(do (require 'kixi.stentor.system)
+             (require 'com.stuartsierra.component)
+             (com.stuartsierra.component/start (kixi.stentor.system/new-system))
+             )))
