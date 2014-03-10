3f84a8587d580080b43b0cc383a0581364a917b0
Malcolm Sparks <malcolm@juxt.pro>
2014-03-10 17:10:07 +0000

    Selectable layers
    This is based on setting a layer to remove and add.

--- src-cljs/kixi/stentor/main.cljs
+++ src-cljs/kixi/stentor/main.cljs
@@ -9,7 +9,10 @@
 
 (def app-model
   (atom
-   {}))
+   {:poi-layer nil
+    :poi-selector [{:label "Layer 1" :value "A"}
+                   {:label "Layer 2" :value "B"}
+                   {:label "Layer 3" :value "C"}]}))
 
 (def tile-url "http://{s}.tile.cloudmade.com/84b48bab1db44fb0a70c83bfc087b616/997/256/{z}/{x}/{y}.png")
 
@@ -37,10 +40,22 @@
               [:select
                {:onChange
                 (fn [e]
-                  (om/update! app-state :poi (.-value (.-target e))))}
+                  (let [value (.-value (.-target e))]
+                    (println "Getting URI" value)
+                    (GET "/geojson.js"
+                        {:handler (fn [json]
+                                    (let [data (clj->js json)
+                                          layer (-> js/L (.geoJson data))]
+                                      (om/update! app-state :layer-to-remove (:poi-layer @app-state))
+                                      (when (= "A" value)
+                                        (om/update! app-state :layer-to-add layer))))
+                         :response-format :json}))
+                  )}
                [:option "None"]
-               [:option {:value "A"} "Layer 1"]
-               [:option {:value "B"} "Layer 2"]]]]))))
+               (for [{:keys [label value]} (:poi-selector app-state)]
+                 [:option {:value value} label]
+                 )
+               ]]]))))
 
 (defn map-component
   "put the leaflet map as state in the om component"
@@ -51,12 +66,7 @@
     (will-mount [this]
       (println "will mount")
       ;; TODO don't use json GETs!! see https://github.com/yogthos/cljs-ajax
-      (GET "/geojson.js"
-          {:handler (fn [e]
-                      (let [data (clj->js e)
-                            layer (-> js/L (.geoJson data))]
-                        (om/update! app-state :poi-layer layer)))
-           :response-format :json}))
+      )
 
     om/IRender
     (render [this]
@@ -73,9 +83,15 @@
       (let [node (om/get-node owner)
             {:keys [leaflet-map] :as map} (om/get-state owner :map)
             ]
-        ;; remove all the layers
-        (when-let [layer (:poi-layer app-state)]
-          (.addLayer leaflet-map layer))
+        (when-let [layer (:layer-to-remove app-state)]
+          (.removeLayer leaflet-map layer)
+          (om/update! app-state :layer-to-remove nil)
+          (om/update! app-state :poi-layer nil))
+
+        (when-let [layer (:layer-to-add app-state)]
+          (.addLayer leaflet-map layer)
+          (om/update! app-state :layer-to-add nil)
+          (om/update! app-state :poi-layer layer))
 
         #_(println "poi value is " (:poi app-state))
         #_(when (= "A" (:poi app-state))
