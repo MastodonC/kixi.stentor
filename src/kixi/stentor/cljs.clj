fbb1fcfa259330d43e6ed2bf0abe72d9e1731969
Malcolm Sparks <malcolm@juxt.pro>
2014-03-10 13:58:05 +0000

    Remove console clutter

--- src/kixi/stentor/cljs.clj
+++ src/kixi/stentor/cljs.clj
@@ -6,10 +6,8 @@
 
 (defn make-routes [config]
   (let [output-dir (str (:output-dir config) "out/")]
-    (println "output dir" output-dir)
     ["/cljs/" (->Files {:dir output-dir
                         :mime-types {"map" "application/javascript"}})]))
 
 (defn new-cljs-routes [config]
-  (println "the config is " config)
   (new-bidi-routes (make-routes config) ""))
