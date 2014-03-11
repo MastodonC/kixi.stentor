845b6dc9cc2e8ea07cae847d819451e012fc5283
Malcolm Sparks <malcolm@juxt.pro>
2014-03-11 11:27:06 +0000

    Add useful refers to user ns

--- dev/user.clj
+++ dev/user.clj
@@ -17,6 +17,9 @@
    [com.stuartsierra.component :as component]
    [clojure.tools.namespace.repl :refer [refresh refresh-all]]
    [kixi.stentor.system :as stentor]
+   [clojure.pprint :refer (pprint)]
+   [clojure.reflect :refer (reflect)]
+   [clojure.repl :refer (apropos dir doc find-doc pst source)]
    modular))
 
 (defn init
