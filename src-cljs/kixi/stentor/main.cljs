346963ef7a21546c96492b36b74c7ea0334569f9
Bruce Durling <bruce@mastodonc.com>
2014-04-01 14:49:15 +0100

    Better Login UX.

--- src-cljs/kixi/stentor/main.cljs
+++ src-cljs/kixi/stentor/main.cljs
@@ -375,6 +375,7 @@
         (om/build postcode-selector-component app-state)
         ;;(om/build map-saver-component app-state)
         ;;(om/build map-loader-component app-state)
+        [:p [:a {:href "/login.html"} "Login"]]
         ]))))
 
 (defn map-component
